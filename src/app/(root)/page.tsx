832cb3f689982023fe9139a210c2824fda0d54d7
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-11-12 23:29:22 +0530

    Sanity live caching and data fetching using ISR

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import IdeaCard, { IdeaCardType } from "@/components/IdeaCard";
 import Navbar from "../../components/Navbar";
-import { client } from "@/sanity/lib/client";
 import { IDEAS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -10,25 +10,35 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(IDEAS_QUERY);
+  //client fetching not for ISR
+  // const posts = await client.fetch(IDEAS_QUERY);
+
+  //new fetch for ISR using "server-only"
+  //it revalidates the page whenever new changes are made
+  const { data: posts } = await sanityFetch({ query: IDEAS_QUERY });
 
   return (
-    <div className="background">
-      <Navbar query={query} />
-      <main className="main_section">
-        <p className="text-2xl text-center mt-8 font-medium text-white">
-          {query ? `Search Results for "${query}"` : "Ideas to explore!"}
-        </p>
-        <div className="section_container">
-          <div className="mt-7 card_grid">
-            {posts?.length > 0 ? (
-              posts.map((post: IdeaCardType) => <IdeaCard key={post?._id} post={post} />)
-            ) : (
-              <p className="no-results">No Ideas Found</p>
-            )}
+    <>
+      <div className="background">
+        <Navbar query={query} />
+        <main className="main_section">
+          <p className="text-2xl text-center mt-8 font-medium text-white">
+            {query ? `Search Results for "${query}"` : "Ideas to explore!"}
+          </p>
+          <div className="section_container">
+            <div className="mt-7 card_grid">
+              {posts?.length > 0 ? (
+                posts.map((post: IdeaCardType) => (
+                  <IdeaCard key={post?._id} post={post} />
+                ))
+              ) : (
+                <p className="no-results">No Ideas Found</p>
+              )}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+      <SanityLive />
+    </>
   );
 }
